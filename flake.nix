9c691202d2953913f34d7f9fce55d28f2e24ad8a
Zahary Karadjov <zahary@metacraft-labs.com>
2026-07-04 13:02:37 +0300

    Standardize repository structure, refactor developer documentation, and add CI workflows

---
+++ flake.nix
@@ -0,0 +1,84 @@
+{
+  description = "Filesystem and process monitoring library and tools";
+
+  inputs = {
+    nixos-modules.url = "github:metacraft-labs/nixos-modules";
+    nixpkgs.follows = "nixos-modules/nixpkgs-unstable";
+    flake-parts.follows = "nixos-modules/flake-parts";
+    git-hooks.follows = "nixos-modules/git-hooks-nix";
+    stackable-hooks-src = {
+      url = "github:metacraft-labs/nim-stackable-hooks";
+      flake = false;
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, nixos-modules, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        nixos-modules.modules.flake.git-hooks
+      ];
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, config, ... }:
+        let
+          version = "0.1.0";
+        in
+        {
+          pre-commit.settings.hooks = {
+            shellcheck.enable = true;
+            nixfmt.enable = true;
+          };
+
+          packages.default = pkgs.stdenv.mkDerivation {
+            pname = "io-mon";
+            inherit version;
+            src = ./.;
+
+            nativeBuildInputs = [
+              pkgs.just
+              pkgs.nim2
+            ];
+
+            STACKABLE_HOOKS_SRC = "${inputs.stackable-hooks-src}/src";
+
+            buildPhase = ''
+              runHook preBuild
+              just build
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              mkdir -p "$out/bin" "$out/lib"
+              if [ -f build/bin/io-mon ]; then
+                install -m755 build/bin/io-mon "$out/bin/io-mon"
+              fi
+              for lib in build/lib/*; do
+                [ -e "$lib" ] || continue
+                install -m755 "$lib" "$out/lib/$(basename "$lib")"
+              done
+              runHook postInstall
+            '';
+          };
+
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ config.pre-commit.devShell ];
+            packages = [
+              pkgs.just
+              pkgs.nim2
+              pkgs.nimble
+              pkgs.git
+              pkgs.nixfmt
+            ];
+          };
+        };
+    };
+}
